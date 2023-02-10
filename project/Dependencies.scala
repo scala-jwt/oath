1ea90df68719b77fc241d45f553f3c35ad13cc32
Scala Steward <scala_steward@virtuslab.com>
2023-02-10 21:53:38 +0000

    Update circe-core, circe-generic, ... to 0.14.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val logbackClassic     = "1.4.5"
     val scalaLogging       = "3.9.5"
     val refined            = "0.10.1"
-    val circe              = "0.14.3"
+    val circe              = "0.14.4"
     val jsoniterScala      = "2.20.6"
     val guava              = "31.1-jre"
     val enumeratum         = "1.7.2"
