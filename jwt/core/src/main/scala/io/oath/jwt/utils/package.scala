c2fb90c9b02b3f60bb180b24a0422cb48fa755b2
Andreas Rigas <48296471+andrewrigas@users.noreply.github.com>
2023-04-15 18:10:04 +0100

    feat: update java-jwt 4.4.0 (#31)

--- jwt/core/src/main/scala/io/oath/jwt/utils/package.scala
+++ jwt/core/src/main/scala/io/oath/jwt/utils/package.scala
@@ -4,13 +4,15 @@ import java.nio.charset.StandardCharsets
 import java.time.Instant
 import java.util.Base64
 
+import com.auth0.jwt.JWTCreator.Builder
 import com.auth0.jwt.interfaces.DecodedJWT
 import eu.timepit.refined.types.string.NonEmptyString
-import io.oath.jwt.model.JwtVerifyError
+import io.oath.jwt.model.{JwtIssueError, JwtVerifyError}
 
 import scala.util.control.Exception.allCatch
 
 import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.util.chaining.scalaUtilChainingOps
 
 package object utils {
 
@@ -50,4 +52,30 @@ package object utils {
       Option(decodedJWT.getNotBefore).map(_.toInstant)
   }
 
+  private[oath] implicit class JWTBuilderOps(private val builder: Builder) {
+
+    private def safeEncode[T](
+        claims: T,
+        toBuilder: String => Builder
+    )(implicit claimsEncoder: ClaimsEncoder[T]): Either[JwtIssueError.EncodeError, Builder] =
+      allCatch
+        .withTry(
+          claimsEncoder
+            .encode(claims)
+            .pipe(toBuilder)
+        )
+        .toEither
+        .left
+        .map(error => JwtIssueError.EncodeError(error.getMessage))
+
+    def safeEncodeHeader[H](claims: H)(implicit
+        claimsEncoder: ClaimsEncoder[H]
+    ): Either[JwtIssueError.EncodeError, Builder] =
+      safeEncode(claims, builder.withHeader)
+
+    def safeEncodePayload[P](claims: P)(implicit
+        claimsEncoder: ClaimsEncoder[P]
+    ): Either[JwtIssueError.EncodeError, Builder] =
+      safeEncode(claims, builder.withPayload)
+  }
 }
