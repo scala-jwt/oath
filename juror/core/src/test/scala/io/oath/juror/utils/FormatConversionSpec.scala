63e1d1a4cfa3f8a4c15814fecafa82c99dbf5c0c
Andreas Rigas <andreas.rigas@outlook.com>
2023-05-24 23:16:39 +0100

    chore: remove guava dep

---
+++ juror/core/src/test/scala/io/oath/juror/utils/FormatConversionSpec.scala
@@ -0,0 +1,12 @@
+package io.oath.juror.utils
+
+import io.oath.jwt.testkit.AnyWordSpecBase
+
+class FormatConversionSpec extends AnyWordSpecBase {
+
+  "FormatConversion" should {
+
+    "create jwt token issuers" in {}
+  }
+
+}
