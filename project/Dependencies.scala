04d43daa0568b114572d9d1349c8c7bb78301e63
Scala Steward <scala_steward@virtuslab.com>
2026-02-16 21:57:50 +0000

    chore: update java-jwt from 4.4.0 to 4.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scalaTest          = "3.2.19"
     val scalaTestPlusCheck = "3.2.17.0"
     val scalacheck         = "1.17.1"
-    val javaJWT            = "4.4.0"
+    val javaJWT            = "4.5.1"
     val config             = "1.4.3"
     val bcprov             = "1.78.1"
     val circe              = "0.14.7"
